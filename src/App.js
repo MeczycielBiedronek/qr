9801d0657def14aab8624d383e6d39cdad83eae3
Męczyciel Biedronek <44778546+MeczycielBiedronek@users.noreply.github.com>
2023-06-15 14:31:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { FaTrash } from 'react-icons/fa';
+import './App.css'
 
 function App() {
+  // const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setImageUrl(`http://api.qrserver.com/v1/create-qr-code/?data=${userInput}&size=200x300`)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {
+      userInput.length > 1
+      ?
+<img src={imageUrl} alt="your qr code" />
+:
+<img src="http://szparowanie.nazwa.pl/strony/react-qr-cra/default.jpg" alt="your qr code" />
+    }
+    
+    <h1>Generate your QR code</h1>
+      <div>
+          <form onSubmit={submitHandler}>
+            <button className="clear-button" onClick={() => {
+              setUserInput('') 
+              
+              }}><FaTrash/> clear</button>
+            <input value={userInput} 
+            onChange={(event) => {setUserInput(event.target.value)
+              submitHandler(event)} }
+            // onKeyDown={(e) => { e.key === 'Enter' && submitHandler(e); }}
+            />
+            {/* <button className="submit-button" type='submit'>get QR code</button> */}
+            
+          </form>
+          <h4>Type text to generate QR code</h4>
+        </div>
+    </>
+  )
 }
 
-export default App;
+export default App
